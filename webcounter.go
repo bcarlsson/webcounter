66690a13b2eb54bc9542ecc07b16b78ee1114273
Björn Carlsson <bjorn@bjorncarlsson.com>
2018-03-13 13:45:03 +0100

    Initial code commit

---
+++ webcounter.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Counter struct {
+	Count int `json:"count"`
+}
+
+func main() {
+	port := flag.String("port", "6000", "Listening port")
+	flag.Parse()
+	myCounter := Counter{
+		Count: 0,
+	}
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", myCounter.Counter)
+	router.HandleFunc("/reset", myCounter.Reset)
+
+	log.Fatal(http.ListenAndServe(":"+*port, router))
+}
+
+func (c *Counter) Counter(w http.ResponseWriter, r *http.Request) {
+	c.Count++
+	json.NewEncoder(w).Encode(c)
+}
+
+func (c *Counter) Reset(w http.ResponseWriter, r *http.Request) {
+	c.Count = 0
+	fmt.Fprint(w, "Counter has been reset\n")
+}
